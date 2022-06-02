b1f230e7bc4055ebcf0a61330faebb21550258f5
Rod Widdowson <rdw@steadingsoftware.com>
2022-06-02 19:29:10 +0100

    foo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,9 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
 /*
  * Licensed to the University Corporation for Advanced Internet Development, 
  * Inc. (UCAID) under one or more contributor license agreements.  See the 
@@ -17,11 +23,29 @@
 
 /** Start Jetty */
 public class Main {
+    private static final String INIT_SQL_HSQLDB="CREATE TABLE StorageRecords (\r\n"
+            + "  context varchar(255) NOT NULL,\n"
+            + "  id varchar(255) NOT NULL,\n"
+            + "  expires bigint DEFAULT NULL,\n"
+            + "  value varchar(255) NOT NULL,\n"
+            + "  version bigint NOT NULL,\n"
+            + "  PRIMARY KEY (context,id)\n"
+            + ")";
 
     /**
      * @param args command-line arguments
+     * @throws SQLException 
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        try (final BasicDataSource dataSource = new BasicDataSource()) {
+            dataSource.setUrl("jdbc:hsqldb:mem:JPAStorageService;hsqldb.sqllog=3");
+            dataSource.setUsername("sa");
+            dataSource.setPassword("");
+            try (final Connection dbConn = dataSource.getConnection()) {
+                final Statement statement = dbConn.createStatement();
+                statement.executeUpdate(INIT_SQL_HSQLDB);
+            }
+        }
         org.eclipse.jetty.start.Main.main(args);
     }
 
